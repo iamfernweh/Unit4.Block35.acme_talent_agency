02d17edfbcb706bbc406d1726b39ac23c00db6da
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 11:49:44 -0400

    fixed bug

--- server/index.js
+++ server/index.js
@@ -1,17 +1,43 @@
-const { client, createTables, createUser, fetchUsers } = require('./db');
+const {
+  client,
+  createTables,
+  createUser,
+  createSkill,
+  createUserSkill,
+  fetchUsers,
+  fetchSkills,
+  fetchUserSkills,
+  destroyUserSkill,
+} = require('./db');
 
 const init = async () => {
-  console.log('connecting to db');
+  console.log('connecting to database');
   await client.connect();
-  console.log('connected to db');
+  console.log('connected to database');
   await createTables();
-  console.log('tables created');
-  const [moe, lucy, larry] = await Promise.all([
-    createUser({ username: 'moe', password: '123' }),
-    createUser({ username: 'lucy', password: '1234' }),
-    createUser({ username: 'larry', password: '12345' }),
-  ]);
+  console.log('tables created.');
+  const [moe, lucy, ethyl, juggling, spinning, yodeling, bartending] =
+    await Promise.all([
+      createUser({ username: 'moe', password: '123' }),
+      createUser({ username: 'lucy', password: '123' }),
+      createUser({ username: 'larry', password: '1234' }),
+      createSkill({ name: 'juggling' }),
+      createSkill({ name: 'spinning' }),
+      createSkill({ name: 'yodeling' }),
+      createSkill({ name: 'bartending' }),
+    ]);
   console.log(await fetchUsers());
+  console.log(await fetchSkills());
+
+  const [lucyYodels, lucyBartends] = await Promise.all([
+    createUserSkill({ user_id: lucy.id, skill_id: yodeling.id }),
+    createUserSkill({ user_id: lucy.id, skill_id: bartending.id }),
+  ]);
+  console.log(await fetchUserSkills(lucy.id));
+
+  await destroyUserSkill(lucyBartends);
+
+  console.log(await fetchUserSkills(lucy.id));
 };
 
 init();
