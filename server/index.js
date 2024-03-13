0ec32528f4785f9ae87aabb45ce177386f028a82
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 11:13:09 -0400

    update db

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const { client, createTables } = require('./db');
+
+const init = async () => {
+  console.log('connecting to db');
+  await client.connect();
+  console.log('connected to db');
+  await createTables();
+  console.log('tables created');
+};
+
+init();
