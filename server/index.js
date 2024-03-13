4cc86ac008558e00d1afec3978786b7e75a25da9
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 12:40:51 -0400

    deployed

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const {
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 //get the skills
 app.get('/api/skills', async (req, res, next) => {
   try {
@@ -40,16 +42,36 @@ app.get('/api/users/:id/userSkills', async (req, res, next) => {
   }
 });
 
+//post
+app.post('/api/users/:userId/userSkills', async (req, res, next) => {
+  try {
+    const userSkill = await createUserSkill({
+      user_id: req.params.userId,
+      skill_id: req.body.skill_id,
+    });
+    res.sendStatus(201).send(userSkill);
+  } catch (er) {
+    next(er);
+  }
+});
+
 //delete users skill
 app.delete('/api/users/:userId/userSkills/:id', async (req, res, next) => {
   try {
-    await deleteUserSkill({ id: req.params.id, user_id: req.params.userId });
+    await destroyUserSkill({ id: req.params.id, user_id: req.params.userId });
     res.sendStatus(204);
   } catch (er) {
     next(er);
   }
 });
 
+//get error message
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).sent({ error: err.message || err });
+});
+
+//init
 const init = async () => {
   console.log('connecting to database');
   await client.connect();
@@ -87,6 +109,9 @@ const init = async () => {
     console.log(`curl localhost:${port}/api/users/${lucy.id}/userSkills`);
     console.log(
       `curl -X DELETE localhost:${port}/api/users/${lucy.id}/userSkills/${lucyYodels.id}`
+    );
+    console.log(
+      `curl -X POST localhost:${port}/api/users/${moe.id}/userSkills -d '{"skill_id": "${spinning.id}"}' -H "Content-Type:application/json"`
     );
   });
 };
